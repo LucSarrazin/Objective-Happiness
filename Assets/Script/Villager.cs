10d97e9ac8b36a04ab20db660ebb6adf958bac23
Luc Sarrazin <luc.sarrazin@free.fr>
2025-12-04 00:00:01 +0100

    Add GameManager and change some config for the villager and add food

--- Assets/Script/Villager.cs
+++ Assets/Script/Villager.cs
@@ -28,8 +28,10 @@ public class Villager : MonoBehaviour
     
     [SerializeField] private bool woodPlace;
     [SerializeField] private bool rockPlace;
-    [SerializeField] private GameObject wood;
-    [SerializeField] private GameObject rock;
+    [SerializeField] private bool foodPlace;
+    [SerializeField] private GameObject[] woodList;
+    [SerializeField] private GameObject[] rockList;
+    [SerializeField] private GameObject[] foodList;
     
     [Header("Villager Objects")]
     private NavMeshAgent agent;
@@ -47,7 +49,9 @@ public class Villager : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        wood = GameObject.FindGameObjectWithTag("Wood");
+        woodList = GameObject.FindGameObjectsWithTag("Wood");
+        rockList = GameObject.FindGameObjectsWithTag("Rock");
+        foodList = GameObject.FindGameObjectsWithTag("Food");
         updateType();
         StartCoroutine(RandomWalk());
     }
@@ -57,37 +61,40 @@ public class Villager : MonoBehaviour
         switch (type)
         {
             case types.vagrant:
-                Debug.Log(name + "villager is vagrant");
+                Debug.Log(name + " villager is vagrant");
                 render1.color = Color.white;
                 render.material.color = Color.white;
                 StopAllCoroutines();
                 break;
             case types.food_picker:
-                Debug.unityLogger.Log(name + "villager is food picker");
+                Debug.unityLogger.Log(name + " villager is food picker");
                 render1.color = Color.blue;
                 render.material.color = Color.blue;
                 StopAllCoroutines();
+                Debug.Log(name + " is now a food picker and going to take food");
+                agent.destination = foodList[Random.Range(0,foodList.Length)].transform.position;
+                foodPlace = true;
                 break;
             case types.lumberjack:
-                Debug.Log(name + "villager is lumberjack");
+                Debug.Log(name + " villager is lumberjack");
                 render1.color = Color.green;
                 render.material.color = Color.green;
                 StopAllCoroutines();
                 Debug.Log(name + " is now a lumberjack and going to take wood");
-                agent.destination = wood.transform.position;
+                agent.destination = woodList[Random.Range(0,woodList.Length)].transform.position;
                 woodPlace = true;
                 break;
             case types.digger:
-                Debug.Log(name + "villager is digger");
+                Debug.Log(name + " villager is digger");
                 render1.color = Color.yellow;
                 render.material.color = Color.yellow;
                 StopAllCoroutines();
                 Debug.Log(name + " is now a digger and going to mine rock");
-                agent.destination = rock.transform.position;
+                agent.destination = rockList[Random.Range(0,rockList.Length)].transform.position;
                 rockPlace = true;
                 break;
             case types.mason:
-                Debug.Log(name + "villager is mason");
+                Debug.Log(name + " villager is mason");
                 render1.color = Color.black;
                 render.material.color = Color.black;
                 StopAllCoroutines();
@@ -98,19 +105,44 @@ public class Villager : MonoBehaviour
     {
         while (woodPlace != true)
         {
-            Debug.Log(name + " start chooping");
-            yield return new WaitForSeconds(5f);
-            Debug.Log(name + " a recupéré " + Random.Range(1,11));
+            if (agent.hasPath == false)
+            {
+                Debug.Log(name + " start chooping");
+                yield return new WaitForSeconds(5f);
+                Debug.Log(name + " get " + Random.Range(1,6));
+                agent.destination = woodList[Random.Range(0,woodList.Length)].transform.position;
+            }
+            yield return null;
         }
     }
     
     IEnumerator miningRock()
     {
-        while (woodPlace != true)
+        while (rockPlace != true)
         {
-            Debug.Log(name + " start minning");
-            yield return new WaitForSeconds(5f);
-            Debug.Log(name + " a recupéré " + Random.Range(1,11));
+            if (agent.hasPath == false)
+            {
+                Debug.Log(name + " start minning");
+                yield return new WaitForSeconds(5f);
+                Debug.Log(name + " get " + Random.Range(1,6));
+                agent.destination = rockList[Random.Range(0,rockList.Length)].transform.position;
+            }
+            yield return null;
+        }
+    }
+    
+    IEnumerator searchingFood()
+    {
+        while (foodPlace != true)
+        {
+            if (agent.hasPath == false)
+            {
+                Debug.Log(name + " start searching food");
+                yield return new WaitForSeconds(5f);
+                Debug.Log(name + " get " + Random.Range(1,6));
+                agent.destination = foodList[Random.Range(0,foodList.Length)].transform.position;
+            }
+            yield return null;
         }
     }
 
@@ -127,18 +159,24 @@ public class Villager : MonoBehaviour
             Debug.Log(name + " is dead from hunger...");
         }
 
+        // Three Fonction to verify that the villager is where there is food/tree/rock and stop there
         if (woodPlace == true && agent.hasPath == false)
         {
             woodPlace = false;
             StopAllCoroutines();
             StartCoroutine("cuttingTree");
         }
-
         if (rockPlace == true && agent.hasPath == false)
         {
             rockPlace = false;
             StopAllCoroutines();
             StartCoroutine("miningRock");
+        }
+        if (foodPlace == true && agent.hasPath == false)
+        {
+            foodPlace = false;
+            StopAllCoroutines();
+            StartCoroutine("searchingFood");
         }
     }
 
