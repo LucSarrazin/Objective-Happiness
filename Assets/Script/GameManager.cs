6f290a1dc0d7eef9ff8cf8436566ee83ef6c16be
Luc Sarrazin <luc.sarrazin@free.fr>
2025-12-04 15:52:36 +0100

    Villager prefab changes, and gamemanager add method

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int days;
+    public int days = 1;
     public float minutes;
-    public int totalWood;
-    public int totalRock;
-    public int totalFood;
-    public int totalProgress;
-    public int numberMason;
-    public int totalPopulation;
+    public int totalWood = 5;
+    public int totalRock = 5;
+    public int totalFood = 5;
+    public int totalProgress = 0;
+    public int numberMason = 1;
+    public int totalPopulation = 5;
     
     // Start is called before the first frame update
     void Start()
     {
         days = 1;
         totalFood = 5;
-        totalRock = 5;
+        totalRock= 5;
         totalWood = 5;
         totalProgress = 0;
         numberMason = 1;
@@ -33,6 +33,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Time.timeScale = 0;
+        }
     }
 }
