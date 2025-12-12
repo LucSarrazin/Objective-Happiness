036e278360c508d0087a3cf9389ed91a83df6c16
valet noir <valetnoir.gaming@gmail.com>
2025-12-12 10:42:55 +0100

    added randomness in environment spirtes

--- Assets/Script/RandomFlip.cs
+++ Assets/Script/RandomFlip.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class RandomFlip : MonoBehaviour
 {
+    [SerializeField] private bool flipOnStart = true;
+    [SerializeField] private bool modifySize = false;
+
     void Start()
     {
-        if (Random.value > 0.5f)
-            transform.localScale = new Vector3(-1, 1, 1);
+        if (modifySize)
+            transform.localScale = new Vector3(Random.Range(0.8f, 1.2f), Random.Range(0.8f, 1.2f), Random.Range(0.8f, 1.2f));
+
+
+        if (flipOnStart && Random.value > 0.5f)
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y * 1, transform.localScale.z * 1);
     }
 }
