9d3795fc8d533a60bcf862670246c60c01ed8d4b
valet noir <valetnoir.gaming@gmail.com>
2025-12-12 09:53:14 +0100

    added back random flip

--- Assets/Script/RandomFlip.cs
+++ Assets/Script/RandomFlip.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class RandomFlip : MonoBehaviour
 {
-    [SerializeField] private List<SpriteRenderer> spriteRenderers;
-
     void Start()
     {
         if (Random.value > 0.5f)
-            for (int i = 0; i < spriteRenderers.Count; i++)
-                spriteRenderers[i].flipX = !spriteRenderers[i].flipX;
+            transform.localScale = new Vector3(-1, 1, 1);
     }
 }
